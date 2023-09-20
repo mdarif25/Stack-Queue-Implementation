347baf9933e73919e0868895da4bad534cdc9348
md arif ahmad <arifreyaz@gmail.com>
2023-09-20 22:26:07 +0530

    Stack & Queue

---
+++ QueueImplementation/UsingArray.cpp
@@ -0,0 +1,56 @@
+//   Queue implementation using array
+// FIFO
+#include<bits/stdc++.h>
+using namespace std;
+int front=-1,rear=-1;
+int arr[6];
+bool isEmpty(){
+    if(rear==front)return true;
+    return false;
+}
+int Size(){
+    return rear-front;
+}
+void Enqueue(int num){
+    if(Size()==6)cout<<"overflow\n";
+    else arr[(++rear)%6]=num;
+}
+
+void Dequeue(){
+     if(isEmpty())cout<<"underflow\n";
+     else front++;
+}
+void  peek(){
+    if(isEmpty())cout<<"queue is empty\n";
+    else cout<<arr[front+1]<<endl;
+}
+int main(){
+    Enqueue(1);
+    Enqueue(2);
+    Enqueue(3);
+    Enqueue(4);
+    Enqueue(5);
+    Enqueue(6);
+    // Enqueue(7);
+    // cout<<isEmpty()<<endl;
+    // cout<<Size()<<endl;
+    Dequeue();
+    // peek();
+    Dequeue();
+    // peek();
+    Dequeue();
+    // cout<<Size()<<endl;
+    // Dequeue();
+    // Dequeue();
+    // Dequeue();
+    // cout<<Size()<<endl;
+    // cout<<isEmpty()<<endl;
+    Enqueue(7);    
+    Enqueue(8);    
+    Enqueue(9); 
+    // cout<<Size()<<endl;
+    cout<<isEmpty()<<endl;   
+    // Enqueue(9);    
+
+    return 0;
+} 
---
+++ stackImplementaion/UsingArray.cpp
@@ -0,0 +1,43 @@
+
+
+// <---------Stack Implementation Using Array----->
+#include<bits/stdc++.h>
+using namespace std;
+int idx=-1;
+int arr[20];
+int Size(){
+     return idx+1;
+}
+bool isEmpty(){
+     if(idx==-1)return true;
+     return false;
+}
+void push(int num){
+     if(Size()==100)cout<<"stack is full\"\"overflow\n";
+     else arr[++idx]=num;
+}
+void pop(){
+     if(isEmpty())cout<<"Stack is empty\"\"underflow\n";
+     else idx--;
+}
+void top(){
+     if(isEmpty())cout<<"underflow\n";
+     else cout<<arr[idx]<<endl;
+}
+int main(){
+     push(10);
+     push(20);
+     push(30);
+     push(40);
+     top();
+     pop();
+     top();
+     pop();
+     top();
+     top();
+     push(100);
+     push(-20);
+     top();
+     cout<<Size()<<endl;
+     return 0;    
+}
